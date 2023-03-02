a138c55cc9721139b359094eb63a44d8e2f8dfed
Christian Rosa <crosa93921@gmail.com>
2023-03-02 15:17:02 -0700

    finsihed phase 3

--- practice-for-week-10-sql-2-long-practice/sql-practice/phase-3/query-1.sql
+++ practice-for-week-10-sql-2-long-practice/sql-practice/phase-3/query-1.sql
@@ -1,2 +1,12 @@
 -- Find Hermione's cats
 -- Your code here
+-- .headers on
+-- .mode columns
+
+SELECT cats.name
+FROM cats
+JOIN cat_owners
+ON (cats.id = cat_owners.cat_id)
+JOIN owners
+ON (owners.id = cat_owners.owner_id)
+WHERE owners.first_name = "Hermione";
--- practice-for-week-10-sql-2-long-practice/sql-practice/phase-3/query-2.sql
+++ practice-for-week-10-sql-2-long-practice/sql-practice/phase-3/query-2.sql
@@ -1,2 +1,10 @@
 -- Find All the Toys for Hermione's cats
 -- Your code here
+
+SELECT toys.name
+FROM toys
+JOIN cat_owners
+ON (toys.cat_id = cat_owners.cat_id)
+JOIN owners
+ON (owners.id = cat_owners.owner_id)
+WHERE owners.first_name = "Hermione";
